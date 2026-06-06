f564da2b3345eed2726c9fcd1b59c2a05929a411
YaoLeiXing <yaoleyxing@gmail.com>
2026-06-06 09:40:05 +0800

    chore: automation round 4 for q-876

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
+  ssr: true,
   server: {
     preset: "node-server",
   },
   vite: {
-    build: {
-      sourcemap: true,
+    resolve: {
+      conditions: ["solid", "development", "browser"],
     },
   },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,1 @@
-import { defineConfig } from "vite";
-import solid from "@solidjs/start/vite";
-
-export default defineConfig({
-  plugins: [solid()],
-});
+export default {};
